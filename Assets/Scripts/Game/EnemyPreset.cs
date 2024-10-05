38a26bb45a3fba3e4acda2bde5276343a96cd3bc
Tim16763 <94013118+RazoV1@users.noreply.github.com>
2024-10-05 19:47:47 +0300

    dementia update

--- Assets/Scripts/Game/EnemyPreset.cs
+++ Assets/Scripts/Game/EnemyPreset.cs
@@ -49,7 +49,7 @@ public class EnemyPreset : MonoBehaviour
 			DamageResistance = Mathf.Clamp(DamageResistance - resistancePenetration,-100,100);
 			HP -= Mathf.Clamp(damage - DamageResistance,0,1000);
 			UpdateViewModels();
-			if (HP - damage - DamageResistance <= 0)
+			if (HP - Mathf.Clamp(damage - DamageResistance,0,1000) <= 0)
 			{
 				HP = 0;
 				UpdateViewModels();
@@ -111,7 +111,7 @@ public class EnemyPreset : MonoBehaviour
 				damageResistance += card.DamageResistance;
 			}
 			honestReaction.PlayNeutral();
-			heal += card.Heal;
+			//heal += card.Heal;
 			if (damage > 0)
 			{
 				outputField.text += "Урон: " + damage.ToString() + "\n";
